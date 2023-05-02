47e5df5e02a674973e8fcebbd2c3c46058b9c7f0
Garrett London <garrett.london2@gmail.com>
2023-05-02 02:49:07 -0400

    examples

--- PlainEnglishBlazor.Shared/Models/WeatherForecast.cs
+++ PlainEnglishBlazor.Shared/Models/WeatherForecast.cs
@@ -12,11 +12,12 @@ namespace PlainEnglishBlazor.Shared.Models
         public DateTime Date { get; set; }
 
         /// <summary>The temperature of the forecast in ISO-whatever format</summary>
-        /// <example>32.8</example>
+        /// <example>32</example>
         [Required]
         public int TemperatureC { get; set; }
 
         /// <summary>Autocalculated farenheit temperature.</summary>
+        /// <example>32</example>
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         /// <summary> The summary of the weather (cold, hot) </summary>
